fa6e9ccf7d20651adee53941123b7aba7ad9d413
luelly <sun.luella@gmail.com>
2019-11-18 18:04:42 -0800

    added some (badly placed) speed boosts

---
+++ Assets/SpeedPad.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class SpeedPad : MonoBehaviour {
+
+    public CinemachineVirtualCamera followCamera;
+
+    [Range(0, 5)]
+    public float duration = 1f;
+    [Range(0,50)]
+    public float boostAmount;
+    public float zoomAmount = 40;
+
+    private float originalFOV;
+
+    private bool isBoostFOV;
+
+    void Awake()
+    {
+        originalFOV = followCamera.m_Lens.FieldOfView;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        var rb = other.attachedRigidbody;
+        if (rb == null) return;
+        var kart = rb.GetComponent<VehicleMovement>();
+        kart.StartCoroutine(KartModifier(kart, duration));
+    }
+
+    IEnumerator KartModifier(VehicleMovement kart, float lifetime)
+    {
+        kart.driveForce += boostAmount;
+        isBoostFOV = true;
+        yield return new WaitForSeconds(lifetime);
+        kart.driveForce -= boostAmount;
+        isBoostFOV = false;
+    }
+
+    void Update()
+    {
+        if (followCamera.m_Lens.FieldOfView != originalFOV)
+        {
+            if (isBoostFOV)
+            {
+                followCamera.m_Lens.FieldOfView = Mathf.Lerp(followCamera.m_Lens.FieldOfView, originalFOV + zoomAmount, Time.deltaTime / duration);
+            }
+            else
+            {
+                followCamera.m_Lens.FieldOfView = Mathf.Lerp(followCamera.m_Lens.FieldOfView, originalFOV, Time.deltaTime / duration);
+            }
+        }
+    }
+}
